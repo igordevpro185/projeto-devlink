e6e294c40728a6d953e207e9754326bc97fa38f7
igordevpro185 <https://github.com/igordevpro185/projeto-devlink>
2023-11-13 10:57:59 -0300

    adição de fuction para troca de avata através do toggle

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 
+//function toggleMode() {
+
+// const body = document.body
+ 
+
+
+
+
+
+//body.classList.add('light')
+
+
+
+//podemos trocar toda a nossa condição if pela função (toggle)
+
+function toggleMode() {
+    
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+
+const img = document.querySelector("#avatar img")
+
+
+    if(html.classList.contains('light')) {
+
+        img.setAttribute('src', './assets/light-profile.png')
+
+    }   else {
+
+        img.setAttribute('src', './assets/dark-profile.png')
+
+    }
+ 
+}
